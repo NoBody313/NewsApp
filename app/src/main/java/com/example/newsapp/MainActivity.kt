badf029b340c1528d549f2681efb1f1ac5ace1c9
Fatih Maulana <85884851+NoBody313@users.noreply.github.com>
2021-10-25 13:03:01 +0700

    Sementara

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val vpNews: ViewPager2 = findViewById(R.id.vp_news)
+
+        val tabs: TabLayout = findViewById(R.id.tabs)
     }
 }
