7c09d1127248fc083f7c3154758fcedb031ffee0
Fatih Maulana <85884851+NoBody313@users.noreply.github.com>
2021-10-26 14:48:48 +0700

    baru

--- app/src/main/java/com/example/newsapp/AllNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/AllNewsFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class AllNewsFragme
+class AllNewsFragment: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -4,14 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private var mSectionPagerAdapter: SectionPagerAdapter? = null
+    private lateinit var vpNews: ViewPager2
+    private lateinit var tabs: TabLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val vpNews: ViewPager2 = findViewById(R.id.vp_news)
+        // Untuk Buuat Toolbar
+        setSupportActionBar(findViewById(R.id.toolbar))
 
-        val tabs: TabLayout = findViewById(R.id.tabs)
+        vpNews = findViewById(R.id.vp_news)
+        tabs = findViewById(R.id.tabs)
+
+        mSectionPagerAdapter = SectionPagerAdapter(this)
+        vpNews.adapter = mSectionPagerAdapter
+
+        val tabList = arrayListOf("All News", "Top", "popular")
+
+        // Build title di tab layout
+        TabLayoutMediator(tabs, vpNews){tab, position ->
+            tab.text = tabList[position]
+        }.attach()
+
+//        val vpNews: ViewPager2 = findViewById(R.id.vp_news)
+//
+//        val tabs: TabLayout = findViewById(R.id.tabs)
     }
 }
