4e018ddcbbfd7a0906e286137e25836b7fe1a4d3
Fatih Maulana <85884851+NoBody313@users.noreply.github.com>
2021-11-02 09:11:53 +0700

    Nampilin title category date dll di All News

--- app/src/main/java/com/example/newsapp/AllNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/AllNewsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class AllNewsFragment: Fragment() {
     override fun onCreateView(
@@ -12,7 +14,12 @@ class AllNewsFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_news, container, false)
+        val view = inflater.inflate(R.layout.fragment_all_news, container, false)
+        val rvNews = view.findViewById<RecyclerView>(R.id.rv_all_news)
+        rvNews.layoutManager = LinearLayoutManager(view.context)
+        rvNews.adapter = NewsAdapter(DataNews.listAllNews)
+
+        return view
     }
 
 }
